50387a064e4bb97d59e24c2ff97eae402443edcf
Gdeon1337 <gdeom1337@gmail.com>
2020-02-05 14:39:39 +0400

    ref

--- app/blueprints/exceptions/view.py
+++ app/blueprints/exceptions/view.py
@@ -2,7 +2,6 @@ from sanic import Blueprint
 from sanic.request import Request
 from sanic.response import json
 from sanic.exceptions import InvalidUsage
-from aioredis.errors import ConnectionClosedError
 
 
 blueprint_exceptions = Blueprint('except', url_prefix='/', strict_slashes=True)
